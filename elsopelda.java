da332a157dddb83ac43f8a00c199c7b5f5fb8ca3
Sajt970720 <157577407+Sajt970720@users.noreply.github.com>
2024-01-24 09:04:43 +0100

    Feladat

---
+++ elsopelda.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class elsopelda {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in)
+        System.out.println("Hello Te szemét");
+        System.out.print("");
+    }
+}
